2ab8db2ae1bf6742a0b3c13e93d9171c10eb5a4f
aaku14n <akarshgdps@gmail.com>
2020-10-04 09:02:33 +0530

    Adding Setup changes

---
+++ completeProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  completeProject
+//
+//  Created by Aakarsh Yadav on 04/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
